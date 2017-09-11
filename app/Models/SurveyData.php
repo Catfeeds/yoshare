f418ae2f9645443315206cda4c5115cccc1649e6
刘锰 <amengdewo@163.com>
2017-09-11 13:05:59 +0800

    恢复误删除代码

---
+++ app/Models/SurveyData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SurveyData extends Model
+{
+    protected $table = 'survey_data';
+
+    protected $fillable = [
+        'survey_id',
+        'survey_item_ids',
+        'comment',
+        'person_name',
+        'person_mobile',
+        'member_name',
+        'ip',
+    ];
+}
---
+++ routes/modules/questions/api.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 问答
+ */
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+
+    $api->group(['namespace' => 'App\Api\Controllers', 'middleware' => 'throttle:600'], function ($api) {
+        $api->get('questions/list', 'QuestionController@lists');
+        $api->get('questions/search', 'QuestionController@search');
+        $api->get('questions/info', 'QuestionController@info');
+        $api->get('questions/detail', 'QuestionController@detail');
+        $api->get('questions/share', 'QuestionController@share');
+        $api->get('questions/comments/create','QuestionController@create');
+    });
+});
