4f2dca7e71db2d6983c6c7aa03cd41c5621e0db1
杨晓业 <williams_young1314@163.com>
2017-09-21 15:38:09 +0800

    完善角色管理和用户管理

--- resources/views/admin/roles/_form.blade.php
+++ resources/views/admin/roles/_form.blade.php
@@ -1,41 +1,45 @@
 <div class="form-group">
-    {!! Form::label('name', '名称:',['class' => 'control-label col-sm-2']) !!}
-    <div class="col-sm-10">
+    {!! Form::label('name', '名称:',['class' => 'control-label col-sm-1']) !!}
+    <div class="col-sm-5">
         {!! Form::text('name', null, ['class' => 'form-control']) !!}
     </div>
 </div>
 
 <div class="form-group">
-    {!! Form::label('description', '描述:', ['class' => 'control-label col-sm-2']) !!}
+    {!! Form::label('description', '描述:', ['class' => 'control-label col-sm-1']) !!}
     <div class="col-sm-10">
         {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
     </div>
 </div>
 
 
-<div class="col-sm-10">
-    @if(isset($perms))
-        <div class="checkbox">
-            @foreach($permissions as $key=>$permission)
-                @if($permissions[$key]->groups == $permissions[$key]->name)
-                    <br/>
-                @endif
-                <label>
-                    <input type="checkbox" {{ in_array($permission->id, $perms) ? 'checked' : '' }} name="permission_id[]"
-                           value="{{$permission->id}}">{{$permission->description}}
-                </label>
-            @endforeach
-        </div>
+<div class="form-group">
+    <label for="role" class="control-label col-sm-1">关联权限</label>
+    <div class="col-sm-10">
+        @if(isset($perms))
+            <div class="checkbox">
+                @foreach($permissions as $key=>$permission)
+                    <br class="{{$loop->index}}-perm" />
+                    <label>
+                        <input type="checkbox" {{ in_array($permission['id'], $perms) ? 'checked' : '' }} name="permission_id[]"
+                               value="{{$permission['id']}}">{{$permission['description']}}
+                    </label>
+                    <input type="hidden" value="{{$permission['group']}}" class="{{$loop->index}}-group">
+                @endforeach
+            </div>
+        @else
+            <div class="checkbox">
+                @foreach($permissions as $key=>$permission)
+                    <br class="{{$loop->index}}-perm" />
+                    <label>
+                        <input type="checkbox" name="permission_id[]" value="{{$permission['id']}}">{{$permission['description']}}
+                    </label>
+                    <input type="hidden" value="{{$permission['group']}}" class="{{$loop->index}}-group">
+                @endforeach
+            </div>
+        @endif
+    </div>
 
-    @else
-        <div class="checkbox">
-            @foreach($permissions as $key=>$permission)
-                <label>
-                    <input type="checkbox" name="permission_id[]" value="{{$permission->id}}">{{$permission->description}}
-                </label>
-            @endforeach
-        </div>
-    @endif
 </div>
 
 <div class="box-footer">
@@ -53,5 +57,17 @@
             format: 'YYYY/MM/DD HH:mm',
             locale: 'zh-cn'
         });
+        var count = '{{$count}}';
+        for(i=0; i<parseInt(count); i++){
+            var p = $('.'+i+'-group').val();
+            var n = $('.'+(i+1)+'-group').val();
+            console.log(p+'-'+n);
+            if( parseInt(p) != parseInt(n) ){
+                $('.'+(i+1)+'-perm').css('display', 'block');
+            }else{
+                $('.'+(i+1)+'-perm').css('display', 'none');
+
+            }
+        }
     });
 </script>
