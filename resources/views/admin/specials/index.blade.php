9cbbf6144165c9f7fe906e4c0471df73ce4d4834
杨晓业 <williams_young1314@163.com>
2017-09-30 11:02:32 +0800

    修改专题页面

--- resources/views/admin/specials/index.blade.php
+++ resources/views/admin/specials/index.blade.php
@@ -7,13 +7,13 @@
                 {{ $module->title }}管理
             </h1>
             <ol class="breadcrumb">
-                <li><a href="/admin/index"><i class="fa fa-dashboard"></i> 首页</a></li>
+                <li><a href="/index"><i class="fa fa-dashboard"></i> 首页</a></li>
                 <li class="active">{{ $module->title }}管理</li>
             </ol>
         </section>
         <section class="content">
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-2">
                     <div class="box box-success">
                         <div class="box-body">
                             <div id="tree">
@@ -21,7 +21,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-10">
                     <div class="box box-info">
                         <div class="box-body">
                             @include('admin.layouts.confirm', ['message' => '您确认删除该条信息吗？'])
