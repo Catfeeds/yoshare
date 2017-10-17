bb97e2f19f1e925db5cf0a9d724c5c9ce74ddf19
humengxu <939631353@qq.com>
2017-10-17 16:29:16 +0800

    问卷管理统计中题目合并单元格

--- modules/survey/views/items.blade.php
+++ modules/survey/views/items.blade.php
@@ -10,7 +10,7 @@
                 <thead>
                 <tr>
                     <th data-field="id" data-align="center" data-width="45">ID</th>
-                    <th data-field="subject">问卷题目</th>
+                    <th data-field="subject" data-align="center">问卷题目</th>
                     <th data-field="title">问卷选项</th>
                     <th data-field="count" data-align="center" data-width="90" data-editable="true">参与数</th>
                     <th data-field="percent" data-align="center" data-width="120">百分比</th>
@@ -28,7 +28,7 @@
                 row._token = '{{ csrf_token() }}';
                 $.ajax({
                     type: "put",
-                    url: "/admin/surveys/items" + row.id,
+                    url: "/admin/surveys/items/" + row.id,
                     data: row,
                     success: function (data, status) {
                         $('#items_table').bootstrapTable('refresh');
@@ -37,7 +37,43 @@
                         alert('Error');
                     },
                 });
+            },
+            onLoadSuccess: function (data) {
+                var data = $('#items_table').bootstrapTable('getData', true);
+                //合并单元格
+                mergeCells(data, "subject", 1, $('#items_table'));
             }
         });
     })();
+
+    /**
+     * 合并单元格
+     * @param data  原始数据（在服务端完成排序）
+     * @param fieldName 合并属性名称
+     * @param colspan   合并列
+     * @param target    目标表格对象
+     */
+    function mergeCells(data, fieldName, colspan, target) {
+        var sortMap = {};
+        for (var i = 0; i < data.length; i++) {
+            for (var j in data[i]) {
+                if (j == fieldName) {
+                    var key = data[i][j];
+                    if (sortMap.hasOwnProperty(key)) {
+                        sortMap[key] = sortMap[key] * 1 + 1;
+                    } else {
+                        sortMap[key] = 1;
+                    }
+                    break;
+                }
+            }
+        }
+        var index = 0;
+        for (var i in sortMap) {
+            var count = sortMap[i] * 1;
+            $(target).bootstrapTable('mergeCells', {index: index, field: fieldName, colspan: colspan, rowspan: count});
+            index += count;
+        }
+    }
+
 </script>
